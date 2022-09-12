e46ad742488ecd8cfe727133a942f8f922b87080
katarinamajsler <113298167+katarinamajsler@users.noreply.github.com>
2022-09-12 16:50:43 +0200

    Zaključna naloga: prvo nalaganje

---
+++ data/Izvir_LjubljaniceVrhnikaRetovlje_krozna_potwaypoints_1.js
@@ -0,0 +1,1 @@
+var json_Izvir_LjubljaniceVrhnikaRetovlje_krozna_potwaypoints_1 = {"type":"FeatureCollection","name":"Izvir_LjubljaniceVrhnikaRetovlje_krozna_potwaypoints_1","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
